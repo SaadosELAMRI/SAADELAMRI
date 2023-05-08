42bf57d4eac756ced412daf9db4e4fe535260bf5
SaadosELAMRI <110356715+SaadosELAMRI@users.noreply.github.com>
2023-05-07 22:30:00 -0400

    first commit

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -4,12 +4,14 @@ export default async function handler(req, res) {
   const { name, email, subject, message } = req.body;
 
   try {
+    console.log("Sending email...");
     // Send email using nodemailer
     await transporter.sendMail({
       ...mailOptions,
       subject: `New message from ${name}: ${subject}`,
       text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
     });
+    console.log("Email sent successfully");
 
     res.status(200).json({ message: "Message sent successfully" });
   } catch (error) {
