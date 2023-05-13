7554e4e0e7e49248d111717c72e1fc5d39388c21
Saad EL-amri <110356715+SaadosELAMRI@users.noreply.github.com>
2023-05-12 22:16:08 -0400

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -18,27 +18,30 @@ const Hero = ({ heading, message }) => {
         
         {/* Splitting icons into two lines */}
         <div className='icons flex flex-wrap justify-center'>
-          <IoLogoHtml5 className='text-orange-500 text-12xl hover:text-orange-700 cursor-pointer' />
-          <SiCss3 className='text-blue-500 text-12xl hover:text-blue-700 cursor-pointer' />
-          <SiJavascript className='text-yellow-500 text-12xl hover:text-yellow-700 cursor-pointer' />
-          <FaJava className='text-white text-12xl hover:text-gray-300 cursor-pointer' />
-          <TbBrandNextjs className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <AiFillAndroid className='text-green-500 text-12xl hover:text-green-300 cursor-pointer'/>
-          <AiOutlineConsoleSql className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <SiMysql className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <SiApachecassandra className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <SiMongodb className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <IoLogoHtml5  className='text-orange-500 text-12xl hover:text-orange-700 cursor-pointer' 
+          title='HTML'/>
+          <SiCss3  title='CSS' className='text-blue-500 text-12xl hover:text-blue-700 cursor-pointer' />
+          <SiJavascript
+            className='text-yellow-500 text-12xl hover:text-yellow-700 cursor-pointer'
+            title='Javascript'
+          />          <FaJava title='JAVA' className='text-white text-12xl hover:text-gray-300 cursor-pointer' />
+          <TbBrandNextjs title='Next.js' className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <AiFillAndroid title='Android Studio' className='text-green-500 text-12xl hover:text-green-300 cursor-pointer' />
+          <AiOutlineConsoleSql title='SQL'className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <SiMysql  title='MySQL'className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <SiApachecassandra title='Cassandra'className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <SiMongodb title='MongoDB' className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
         </div>
         
         <div className='icons flex flex-wrap justify-center'>
-          <FcLinux/>
-          <TbBrandCSharp className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <AiFillGithub className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <FaPython className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <SiMicrosoftazure className='text-blue-500 text-12xl hover:text-gray-300 cursor-pointer'/>
-          <SiCisco className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <SiWindows className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
-          <SiPowershell className='text-blue-400 text-12xl hover:text-gray-300 cursor-pointer'/>
+          <FcLinux title='Linux'/>
+          <TbBrandCSharp title='C#' className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <AiFillGithub  title='Github' className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <FaPython title='Python' className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <SiMicrosoftazure title='Microsoft Azure' className='text-blue-500 text-12xl hover:text-gray-300 cursor-pointer'/>
+          <SiCisco title='Cisco' className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <SiWindows title='Windows 10' className='text-white text-12xl hover:text-gray-300 cursor-pointer'/>
+          <SiPowershell title='Windows PowerShell' className='text-blue-400 text-12xl hover:text-gray-300 cursor-pointer'/>
         </div>
       </div>
     </div>
